a273f464a5cf87ad0da7b15a70e58a2eb627fb4a
Linx <646044080@qq.com>
2022-05-04 12:35:37 +0800

    ros2_control

---
+++ README.md
@@ -0,0 +1,21 @@
+# Closed loop demo for ROS2
+
+It is a ROS2 version of the Four-bar linkage example.
+
+In ROS2, spawning a entity in Gazebo must be done by generating from the topic of "robot_state_publisher" or a SDF file. Luckily the origin method can still be used.
+
+
+
+Due to URDF cannot express closed loop structure, I use some tricks to show closed loop structure in rviz.
+
+To do so, you must have a real closed loop structure robot, or you can realize it through classic gazebo. Implementation with ignition gazebo has not been tested.
+
+Firstly you need to break the closed loop chain, to make your URDF a open loop structure. You can easily do it through remove a joint of a closed loop.
+
+Then, Inject this joint with <gazebo> tag in URDF file. As we spawn the model in gazebo through robot_description topic, we will get a closed loop robot in gazebo.
+
+Next we need to use ros2_control, more specifically, gazebo_ros2_control to show the robot in rviz. It does a map from the joint state in gazebo to the ros2 topic.
+
+With above steps, you can visualize your closed loop robot in both gazebo and rviz, and they are synchronized. An issues is when you restart the world, synchronization will lost.
+
+Further, Moveit and Nav may can be applied on closed loop robot. However one thing should be keeped in mind: it still a open loop robot in ros2. 
---
+++ launch/closed_loop.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription, launch_description
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+import os
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]), 
+            ' ',
+            PathJoinSubstitution([FindPackageShare('closed_loop'), 'urdf', 'closed_loop.xacro']),
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros,"launch","gazebo.launch.py"))
+    )
+
+    gazebo_spawner = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=['-entity', 'closed_loop', '-topic', 'robot_description']
+        )
+
+    controllers_config = PathJoinSubstitution(
+        [
+            FindPackageShare("closed_loop"),
+            "ros2_control_config",
+            "robot_state_publisher.yaml",
+        ]
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    nodes = [
+        robot_state_pub_node,
+        gazebo_spawner,
+        load_joint_state_controller,
+    ]
+
+    return LaunchDescription(
+        nodes +  [gazebo_launch])
